f6d89304fe8a2325024ea5f77bcbe364b42f3e6e
klBiazon <kevin.biazon18@gmail.com>
2020-04-11 16:00:55 -0400

    Added components on the app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { productsRoute } from './products/products.route';
+import { homeRoute } from './home/home.route';
 
-const routes: Routes = [];
+const routes: Routes = [homeRoute, ...productsRoute];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { MainComponent } from './layouts/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
