d9b0983e27ae5d9f4caf17b3691693859653155c
klBiazon <kevin.biazon18@gmail.com>
2020-04-11 15:59:44 -0400

    Added product-list component

---
+++ src/app/products/product-list/product-list.route.ts
@@ -0,0 +1,8 @@
+import { Route } from '@angular/router';
+
+import { ProductListComponent } from './product-list.component';
+
+export const productListRoute: Route = {
+  path: 'products',
+  component: ProductListComponent
+};
