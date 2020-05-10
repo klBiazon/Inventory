c28ad6e6b958899512ab791dce1d40f7411e0e24
klBiazon <kevin.biazon18@gmail.com>
2020-05-10 17:53:42 -0400

    Added product form page

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayImageService } from './../../services/display-image.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Products } from './../products.model';
+import { ProductsService } from '../products.service';
+import { mimeType } from './../../services/mime-type.validator';
 
 @Component({
   selector: 'app-product-form',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
 
-  constructor() { }
+  products: Products;
+
+  form: FormGroup;
+
+  constructor(public displayImageService: DisplayImageService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'name': new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      'imgUrl': new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+
+    // this.form.setValue({
+    //   'title': 'Sample',
+    //   'imgUrl': 'image'
+    // });
   }
 
+  imagePicked(file: File) {
+    if (file) {
+      this.form.patchValue({imgUrl: file});
+      this.form.updateValueAndValidity();        
+    }
+  }
+
+  submit(form) {
+    console.log(form);
+  }
 }
