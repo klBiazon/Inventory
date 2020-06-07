740402cb04f6b799896fb1e54362da1c70eb44d6
klBiazon <kevin.biazon18@gmail.com>
2020-06-07 19:55:30 -0400

    Changed the imgUrl field to required

--- backend/products/products.model.js
+++ backend/products/products.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
-  imgUrl: { type: String }
+  imgUrl: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
