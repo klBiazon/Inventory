cf7530d0d7dd665b690318cc9cd273a28850deec
klBiazon <kevin.biazon18@gmail.com>
2020-06-07 19:49:57 -0400

    Iterate productFormRoute as it becomes 'Routes'

--- src/app/products/products.route.ts
+++ src/app/products/products.route.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import { productListRoute } from './product-list/product-list.route';
 import { productFormRoute } from './product-form/product-form.route';
 
-const PRODUCT_ROUTES = [productListRoute, productFormRoute];
+const PRODUCT_ROUTES = [productListRoute, ...productFormRoute];
 
 export const productsRoute: Routes = [
     {
