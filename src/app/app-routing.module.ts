69da2c90b90a94e3a4f4fd58122a6f1259e4a55f
klBiazon <kevin.biazon18@gmail.com>
2020-06-12 21:49:24 -0400

    Added signup and login route to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { productsRoute } from './products/products.route';
 import { homeRoute } from './home/home.route';
+import { loginRoute } from "./auth/login/login.route";
+import { signupRoute } from "./auth/signup/signup.route";
 
-const routes: Routes = [homeRoute, ...productsRoute];
+const routes: Routes = [homeRoute, ...productsRoute, loginRoute, signupRoute];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
