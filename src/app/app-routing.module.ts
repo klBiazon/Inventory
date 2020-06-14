bb70d237b44797c03294b7a62c8f900660a80d04
klBiazon <kevin.biazon18@gmail.com>
2020-06-14 19:43:34 -0400

    Added AuthGuard to app providers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ import { productsRoute } from './products/products.route';
 import { homeRoute } from './home/home.route';
 import { loginRoute } from "./auth/login/login.route";
 import { signupRoute } from "./auth/signup/signup.route";
+import { AuthGuard } from './auth/auth-guard';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [homeRoute, ...productsRoute, loginRoute, signupRoute];
+const routes: Routes = [homeRoute, ...productsRoute, loginRoute, signupRoute,
+  { path: '**', component: HomeComponent } // Redirect if the URL is not registered within the app
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
