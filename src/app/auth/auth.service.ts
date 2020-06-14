bb686a2bec925f87b67bf95d0d83422160d174fe
klBiazon <kevin.biazon18@gmail.com>
2020-06-14 19:41:15 -0400

    Added functions that stores the token in local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/services/api.service';
 
 import { AuthData } from './auth-data.model';
-import { tokenName } from '@angular/compiler';
 import { ErrorHandlerService } from '../services/error-handler.service';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
@@ -12,13 +13,25 @@ import { ErrorHandlerService } from '../services/error-handler.service';
 export class AuthService extends ApiService {
 
   private token: string;
+  private isAuthenticated = false;
+  private authStatusListener = new Subject<boolean>();
+  private tokenTimer;
   
-  constructor(http: HttpClient, private errorHandlerService: ErrorHandlerService) {
+  constructor(http: HttpClient, private errorHandlerService: ErrorHandlerService,
+    private router: Router) {
     super('http://localhost:3000/api/user', http)
   }
 
   getToken() {
     return this.token;
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(user: AuthData) {
@@ -37,6 +50,71 @@ export class AuthService extends ApiService {
       .subscribe(res => {
         const token = res['token'];
         this.token = token;
+        if(token) {
+          this.authStatusListener.next(true);
+          this.isAuthenticated = true;
+
+          //will logout automatically based on the expiresIn duration
+          const expiresIn = res['expiresIn'];
+          this.setAuthTimer(expiresIn);
+
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + (expiresIn * 1000));
+          this.saveAuthData(this.token, expirationDate);
+          this.router.navigate(['/']);
+        }  
       }, error => this.errorHandlerService.handleError(error));;
+  }
+
+  logout() {
+    clearTimeout(this.tokenTimer);
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.clearAuthData();
+    this.router.navigate(['/']);
+  }
+
+  autoAuthUser() {
+    const authInfo = this.getAuthData();
+    if (!authInfo) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInfo.expirationDate.getTime() - now.getTime();
+    if(expiresIn > 0) {
+      this.token = authInfo.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
+  private setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(()=> {
+      this.logout();
+    }, (duration * 1000));
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if(!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    };
   }
 }
