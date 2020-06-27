c49dce4f2576c86d49bf17ee2bc97331fc64dec0
klBiazon <kevin.biazon18@gmail.com>
2020-06-26 22:49:15 -0400

    Make a request 'tokenData' key which holds the decoded token

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, 'this_is_for_temporary');
+    const decodedToken = jwt.verify(token, 'this_is_for_temporary');
+    req.tokenData = decodedToken;
     next();
   } catch (error) {
     res.status(401).json({ message: "Authentication failed" });
