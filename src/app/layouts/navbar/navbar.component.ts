4ffea7568cc412671653a2b0782c4e32f47b7f03
klBiazon <kevin.biazon18@gmail.com>
2020-06-26 22:46:48 -0400

    Added 'getUser' function to get user's data when logged in

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,26 +1,42 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { Subscription } from 'rxjs';  
+import { ApiService } from 'src/app/services/api.service'
+import { Subscription, Observable } from 'rxjs';  
+import { HttpClient } from '@angular/common/http';
+import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.less']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
+export class NavbarComponent extends ApiService  implements OnInit, OnDestroy {
   
   private authListenerSubs: Subscription;
-
+  private userId: string;
+  
   userAuthenticated = false;
+  userData: Object = {
+    firstName : '',
+    lastName: '',
+    email: ''
+  };
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private errorHandlerService: ErrorHandlerService,
+          http: HttpClient) { 
+    super('http://localhost:3000/api/user', http);
+  }
 
   ngOnInit() {
     this.userAuthenticated = this.authService.getIsAuth();
+    this.userId = this.authService.getUserId();
     this.authListenerSubs = this.authService.getAuthStatusListener()
-      .subscribe(isAuthenticated => {
-        this.userAuthenticated = isAuthenticated;
+      .subscribe(res => {
+        this.userId = res.userId;
+        this.userAuthenticated = res.isAuthenticated;
+        this.getUser();
       });
+    this.getUser();
   }
 
   ngOnDestroy() {
@@ -29,5 +45,14 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   onLogout() {
     this.authService.logout();
+  }
+
+  getUser() {
+    if(this.userId) {
+      this.get(this.userId)
+        .subscribe(user => {
+          this.userData = user;
+        }, error => this.errorHandlerService.handleError(error));
+    }
   }
 }
