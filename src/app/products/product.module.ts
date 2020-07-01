325eeaba45e14f873391fd45a80f63664e5d61cd
klBiazon <kevin.biazon18@gmail.com>
2020-06-30 20:43:40 -0400

    Created a separate module for Product

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductsRoutingModule } from './products.routing.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductFormComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RouterModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductModule { }
