a3460e8ee4a7a3b696b3192693192e48c4ecac95
klBiazon <kevin.biazon18@gmail.com>
2020-06-30 20:46:20 -0400

    Changed path for product adding and editing

--- src/app/products/product-form/product-form.route.ts
+++ src/app/products/product-form/product-form.route.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
 import { ProductFormComponent } from './product-form.component';
-import { AuthGuard } from 'src/app/auth/auth-guard';
 
 export const productFormRoute: Routes = [
-  { path: 'products/add', component: ProductFormComponent, canActivate: [AuthGuard] },
-  { path: 'products/edit/:product_id', component: ProductFormComponent, canActivate: [AuthGuard] }
+  { path: 'add', component: ProductFormComponent },
+  { path: 'edit/:product_id', component: ProductFormComponent }
 ];
