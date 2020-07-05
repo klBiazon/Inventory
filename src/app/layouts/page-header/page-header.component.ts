b601085277faf6b82a083fca37e3dca87ed592a1
klBiazon <kevin.biazon18@gmail.com>
2020-07-05 14:20:52 -0400

    Added a page header component

---
+++ src/app/layouts/page-header/page-header.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, OnDestroy, AfterViewInit, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
+import { Subscription, Subject, BehaviorSubject } from 'rxjs';
+import { LayoutsService } from '../layouts.service';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.less']
+})
+export class PageHeaderComponent implements OnInit, OnDestroy, AfterViewInit, AfterViewChecked {
+
+  private pageHeaderSubs: Subscription;
+  pageHeader: string;
+
+  isLoggedIn: boolean = false;
+  private authListenerSubs: Subscription;
+
+  constructor(private layoutsService: LayoutsService,
+              private authService: AuthService,
+              private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.isLoggedIn = authStatus.isAuthenticated;
+      });
+    this.isLoggedIn = this.authService.getIsAuth();
+  }
+
+  ngAfterViewInit() {
+    this.pageHeaderSubs = this.layoutsService.getPageHeaderListener()
+      .subscribe(value => {
+        this.pageHeader = value;
+      });
+    this.pageHeader = this.layoutsService.getPageHeader();
+  }
+
+  ngAfterViewChecked() {
+    this.cdr.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.pageHeaderSubs.unsubscribe();
+    this.authListenerSubs.unsubscribe();
+  }
+}
