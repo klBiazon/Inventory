cdb790e1b6c3a72ea387cc7b837b65b39b0622fa
klBiazon <kevin.biazon18@gmail.com>
2020-07-12 14:23:13 -0400

    Removed FooterComponent and HomeComponent on app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './products/product.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { MainComponent } from './layouts/main/main.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
-import { AuthModule } from './auth/auth.module';
 
-import { ProductModule } from './products/product.module';
-import { LoadingComponent } from './layouts/loading/loading.component';
 import { PageHeaderComponent } from './layouts/page-header/page-header.component';
+import { LoadingComponent } from './layouts/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    FooterComponent,
     MainComponent,
-    LoadingComponent,
-    PageHeaderComponent
+    NavbarComponent,
+    PageHeaderComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
