bde4fbd42df505b095b6967269c371809843e079
klBiazon <kevin.biazon18@gmail.com>
2020-07-12 14:12:20 -0400

    Used the environment constant 'SERVER_URL' on api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,31 +4,31 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ApiService {
-  constructor(@Inject(String) private SERVER_URL: string, private http: HttpClient) { }
+  constructor(@Inject(String) private apiURL: string, private http: HttpClient) { }
 
   get(id ?: string, pagination ?: any) {
-    return this.http.get(this.SERVER_URL + (id ? '/' + id : '') + 
+    return this.http.get(this.apiURL + (id ? `/${id}` : '') + 
       (pagination ? `?page=${pagination?.page}&pageSize=${pagination?.pageSize}` : ''))
       .pipe(map(res => res));
   }
 
   post(postObj: FormData) {
-    return this.http.post(this.SERVER_URL, postObj)
+    return this.http.post(this.apiURL, postObj)
       .pipe(map(res => res));
   }
 
   put(updateObj) {
-    return this.http.put(this.SERVER_URL + '/' + updateObj.get('id'), updateObj)
+    return this.http.put(this.apiURL + '/' + updateObj.get('id'), updateObj)
       .pipe(map(res => res));
   }
 
   delete(id) {
-    return this.http.delete(this.SERVER_URL + '/' + id)
+    return this.http.delete(this.apiURL + '/' + id)
       .pipe(map(res => res));
   }
 
   user(action: string, userObj) {
-    return this.http.post(this.SERVER_URL + '/' + action, userObj)
+    return this.http.post(this.apiURL + '/' + action, userObj)
       .pipe(map(res => res));
   }
 }
