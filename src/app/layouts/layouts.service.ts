8acc831e182731258ccf15002dff95887b1e5249
klBiazon <kevin.biazon18@gmail.com>
2020-07-12 14:21:49 -0400

    Added significant variables for pagination

--- src/app/layouts/layouts.service.ts
+++ src/app/layouts/layouts.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
+import { FunctionCall } from '@angular/compiler';
+import { data } from 'jquery';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,16 @@ export class LayoutsService {
   private pageHeaderListener = new Subject<string>();
   private pageHeader: string;
 
-  
   private isLoadingListener = new Subject<boolean>();
   private isLoading;
+
+  private countForPaginationListener = new Subject<{ totalCount: number, dataLength: number }>();
+  private count: Object;
+  private functionCallListener = new Subject<any>();
+  private paginationParams = {
+    page: 1,
+    pageSize: 5
+  };
 
   constructor() { }
 
@@ -39,5 +48,42 @@ export class LayoutsService {
 
   getIsLoading(): boolean {
     return this.isLoading;
+  }
+
+  getCountPaginationListener(): Observable<{ totalCount: number, dataLength: number }> {
+    return this.countForPaginationListener.asObservable();
+  }
+
+  getCountPagination() {
+    return this.count;
+  }
+
+  setCountPagination(totalCount: number, dataLength: number) {
+    if(totalCount && dataLength) {
+      this.count = {
+        'totalCount': totalCount,
+        'dataLength': dataLength
+      };
+      this.countForPaginationListener.next({totalCount, dataLength});  
+    }
+  }
+
+  getPaginationEvent(): Observable<any>{ 
+    return this.functionCallListener.asObservable();
+  }
+
+  setPaginationEvent(paginationParams) {
+    this.functionCallListener.next(paginationParams);
+  }
+
+  getPagination() {
+    return this.paginationParams;
+  }
+
+  setPagination(value: {page: number, pageSize: number}) {
+    this.paginationParams = {
+      'page': value.page,
+      'pageSize': value.pageSize
+    };
   }
 }
