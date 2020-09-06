3af11d9a041dc32e2e7035d9636faa9fd45e0de0
KEVIN BIAZON <kevin.biazon18@gmail.com>
2020-09-05 22:51:12 -0400

    Used NgbModal

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { passwordChecker } from 'src/app/services/password-checker';
 import { Router } from '@angular/router';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-signup',
@@ -15,10 +16,13 @@ export class SignupComponent implements OnInit {
   form: FormGroup;
   seePass = false;
   seeConfirmPass = false;
+  
+  @ViewChild('success') successModal: NgbModalRef;
 
   constructor(private authService: AuthService,
     private errorHandlerService: ErrorHandlerService,
-    private router: Router) { }
+    private router: Router,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
     if(this.authService.getIsAuth()) {
@@ -37,7 +41,7 @@ export class SignupComponent implements OnInit {
       }),
       'passwordGroup': new FormGroup({
         'password': new FormControl(null, {
-          validators: [Validators.required]
+          validators: [Validators.required, Validators.minLength(3)]
         }),
         'confirmPassword': new FormControl(null, {
           validators: [Validators.required]
@@ -61,12 +65,21 @@ export class SignupComponent implements OnInit {
     };
     this.authService.createUser(signUpData)
       .subscribe(res => {
-        document.getElementById('showSuccessModal').click();
+        this.form.reset();
+        this.openModal();
       }, error => {
         if('email' in error.error.errors) {
           form.form.controls['email'].setErrors({emailInUse: true});
         }
         this.errorHandlerService.handleError(error);
       });
+  }
+  
+  openModal() {
+    const modalRef = this.modalService.open(this.successModal).result.then((result) => {
+      this.router.navigate(['/login']);
+    }, (reason) => {
+      this.router.navigate(['/login']);
+    });
   }
 }
