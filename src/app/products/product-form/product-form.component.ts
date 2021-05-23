41ca47e9f04199c7cfea6848b7315d6dc198b1dc
KEVIN BIAZON <kevin.biazon18@gmail.com>
2021-05-23 19:01:49 -0400

    Added price, description and list of categories on product form

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef, TemplateRef } from '@angular/core';
 import { DisplayImageService } from './../../services/display-image.service'
-import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormGroupDirective, FormArray } from '@angular/forms';
 
 import { ProductsService } from '../products.service';
 import { mimeType } from './../../services/mime-type.validator';
@@ -8,6 +8,7 @@ import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 import { LayoutsService } from 'src/app/layouts/layouts.service';
 import { Subscription } from 'rxjs';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryService } from 'src/app/category/category.service';
 
 
 @Component({
@@ -18,14 +19,15 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 export class ProductFormComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
-  formValues;
   product_id;
   productInfo;
   formMode = 'Add';
   defaultImage = './../../../assets/defaultImg.png';
   stillLoading = false;
+  categoryList;
 
   private getProductSubs: Subscription;
+  private getCategoriesSubs: Subscription;
 
   @ViewChild('confirmation') confirmationModal: NgbModalRef;
 
@@ -34,20 +36,11 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private router: Router,
     private layoutsService: LayoutsService,
-    private modalService: NgbModal) { }
+    private modalService: NgbModal,
+    private categoryService: CategoryService) { }
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      'name': new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3)]
-      }),
-      'imgUrl': new FormControl(null, {
-        validators: [Validators.required],
-        asyncValidators: [mimeType]
-      })
-    });
-    this.setSubscriptions();
-    this.resetForm();
+    this.setForm();
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has("product_id")) {
         this.formMode = 'Save Changes';
@@ -60,10 +53,34 @@ export class ProductFormComponent implements OnInit, OnDestroy {
         this.product_id = null;
       }
     });
+    this.setSubscriptions();
+    this.categoryService.getCategories();
   }
 
   ngOnDestroy() {
     this.getProductSubs.unsubscribe();
+    this.getCategoriesSubs.unsubscribe();
+  }
+
+  get categories() {
+    return <FormArray>this.form.get("categories");
+  }
+
+  setForm() {
+    this.form = new FormGroup({
+      'name': new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      'price': new FormControl('', {
+        validators: [Validators.required]
+      }),
+      'description': new FormControl(null),
+      'categories': new FormArray([]),
+      'imgUrl': new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
   }
 
   setSubscriptions() {
@@ -87,13 +104,40 @@ export class ProductFormComponent implements OnInit, OnDestroy {
           }
         }
       });
+    this.getCategoriesSubs = this.categoryService.getCategoriesListener()
+      .subscribe(res => {
+        this.categoryList = res.categories;
+      });
   }
 
   setFormValues(val) {
+    val.categories.map(category => {
+      this.categories.push(new FormControl(category));
+    });
     this.form.setValue({
       'name': val.name,
-      'imgUrl': val.imgUrl
+      'imgUrl': val.imgUrl,
+      'description': val.description,
+      'price': val.price,
+      'categories': this.categories.value
     });
+  }
+
+  categoryChecked(e) {
+    if (e.target.checked) {
+      this.categories.push(new FormControl(e.target.value));
+    } else {
+      this.categories.controls.map((item: FormControl, i) => {
+        if (item.value == e.target.value) {
+          this.categories.removeAt(i);
+          return;
+        }
+      });
+    }
+  }
+
+  setCategoryChecked(categoryId) {
+    return this.categories.value.includes(categoryId);
   }
 
   imagePicked(file: File) {
@@ -104,34 +148,42 @@ export class ProductFormComponent implements OnInit, OnDestroy {
   }
 
   submit(form: FormGroupDirective) {
-    if(form.invalid) {
+    if(form.invalid || form.value.categories.length === 0) {
       return;
     }
 
     this.stillLoading = true;
     const productData = new FormData();
+    productData.append("name", form.value.name);
+    productData.append("description", form.value.description ?? '');
+    productData.append("price", form.value.price);
+    form.value.categories.map(category => {
+      productData.append("categories", category);
+    });
+
     if (this.formMode === 'Add') {
-      productData.append("name", form.value.name);
       productData.append("imgUrl", form.value.imgUrl, form.value.name);
       this.productsService.addProduct(productData);
     } else {
       productData.append("id", this.product_id);
-      productData.append("name", form.value.name);
       if (typeof form.value.imgUrl === 'string') {
         productData.append("imgUrl", form.value.imgUrl);
       } else {
         productData.append("imgUrl", form.value.imgUrl, form.value.name);
       }
-      this.formValues = form;
       this.productsService.updateProduct(productData);
     }
   }
 
   resetForm() {
+    this.categories.clear();
     this.form.reset();
     this.form.setValue({
       'name': null,
-      'imgUrl': null
+      'imgUrl': null,
+      'price': null,
+      'description': null,
+      'categories': []
     });
     this.displayImageService.resetDisplayImageService();
   }
