bb270e117e133e84b9938b463e6cbbbf3d6c498b
KEVIN BIAZON <kevin.biazon18@gmail.com>
2021-05-23 18:56:38 -0400

    Added PaginationModule on imports for ProductModule

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsRoutingModule } from './products.routing.module';
-import { PaginationComponent } from '../layouts/pagination/pagination.component';
+import { PaginationModule } from "../layouts/pagination/pagination.module";
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductFormComponent,
-    PaginationComponent
+    ProductFormComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     NgbModule,
     RouterModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    FormsModule,
+    PaginationModule
   ]
 })
 export class ProductModule { }
