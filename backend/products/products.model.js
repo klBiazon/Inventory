f517a96c1a72a618c4af60526b8792c84711b2fe
KEVIN BIAZON <kevin.biazon18@gmail.com>
2021-05-23 20:49:07 -0400

    Added categories, price, description, dateCreated fields on Product model schema

--- backend/products/products.model.js
+++ backend/products/products.model.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
-  imgUrl: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true }
+  categories: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Category', require: true }
+  ],
+  price: { type: String, require: true },
+  description: { type: String },
+  imgUrl: { type: String },
+  dateCreated: { type: Date, required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
